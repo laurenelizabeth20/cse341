80fe58c30e03c2bf0d0b9b5f63a682d497b29b30
laurenelizabeth20 <laurensore@gmail.com>
2022-01-29 19:38:22 -0700

    one more time

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(errorController.get404);
 
 mongoose
   .connect(
-    'mongodb+srv://maximilian:9u4biljMQc4jjqbe@cluster0-ntrwp.mongodb.net/shop?retryWrites=true'
+    'mongodb+srv://new-user:Dstasl66@cluster0.jzlkh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
   )
   .then(result => {
     User.findOne().then(user => {
@@ -50,7 +50,7 @@ mongoose
         user.save();
       }
     });
-    app.listen(PORT);
+    app.listen(PATH);
   })
   .catch(err => {
     console.log(err);
